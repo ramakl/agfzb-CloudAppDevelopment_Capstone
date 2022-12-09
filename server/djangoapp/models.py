da3cb66a09ba5a98bb6c87689af9505fc0e6310c
Rama-kl <rama.m.klaho@gmail.com>
2022-12-09 16:04:49 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,12 +3,34 @@ from django.utils.timezone import now
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30, default='jj')
+    Description = models.CharField(null=False, max_length=30, default='jjj')
+    dob = models.DateField(null=True)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " " + self.Description
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    name = models.CharField(null=False, max_length=100, default='hg')
+    dealer_id = models.IntegerField(max_length=500)
+    Dealer_Type =models.CharField(max_length=500)
+    year=models.DateField()
+    # Many-To-Many relationship with Instructor
+    CarMake = models.ManyToManyField(CarMake)
+    
+    # Create a toString method for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+            "dealer_id: " + self.dealer_id + \
+            "Dealer_Type: " + self.Dealer_Type + \
+            "year: " + self.year
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
